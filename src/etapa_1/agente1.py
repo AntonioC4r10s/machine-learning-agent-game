0779501c306a172c90dae46203c4c6be6c8eda3f
Antonio Carlos <jhunny.tec@gmail.com>
2023-05-18 15:35:33 -0300

    organizar etapa 1

---
+++ src/etapa_1/agente1.py
@@ -0,0 +1,93 @@
+from etapa_1.mapa import Mapa
+import random
+
+class Agente():
+    def __init__(self, mapa:Mapa):
+        self.x = 0
+        self.y = 0
+        self.mapa = mapa
+        self.mapa.posicao_agente(self.x, self.y)
+        self.percepcoes, self.caminhos, self.casa = mapa.info(self.x, self.y)
+        self.flecha = 1
+        
+        self.game_over = False
+        self.ouro = 0
+        self.capturado = False
+        self.caiu = False
+        self.passos = 0
+        self.matou = False
+        self.tiros = 0
+        
+
+    def acao(self):
+        percepcoes = self.percepcoes
+        
+        if percepcoes == []:
+            self.caminha()
+        
+        if percepcoes.count('brilho'):
+            self.ouro += 1
+            self.percepcoes.remove('brilho')
+        
+        elif percepcoes.count('brisa'):
+            self.caminha()
+
+        elif percepcoes.count('fedor'):
+            opcao = random.randint(0, 1)
+
+            if opcao == 0:
+                self.caminha()
+            else:
+                self.atirar()
+
+
+    def caminha(self):
+        proximos = self.caminhos
+        proximo = random.randint(0, len(proximos) - 1 )
+        proxima_casa = proximos[proximo]
+
+        self.mapa.esvazia(self.x, self.y)
+
+        self.x = proxima_casa[0]
+        self.y = proxima_casa[1]
+        self.percepcoes, self.caminhos, self.casa = self.mapa.info(self.x, self.y)
+        self.mapa.posicao_agente(self.x, self.y)
+
+    
+    def atirar(self):
+        # print('tei!')
+
+        if self.flecha <= 0:
+            self.caminha()
+        
+        else:
+
+            alvos = self.caminhos
+            alvo = random.randint(0, len(alvos) - 1 )
+            alvo = alvos[alvo]
+
+            self.flecha -= 1
+            self.tiros += 1
+
+            # print(self.mapa.matriz[alvo[0]][alvo[1]])
+            
+            if self.mapa.matriz[alvo[0]][alvo[1]] == ('M'):
+                self.mapa.esvazia(alvo[0], alvo[1])
+                self.percepcoes.remove('fedor')
+                self.matou = True
+
+        
+    
+    def __str__(self):
+        string = 'Agente: \n'
+        string += f'Percepcões: {self.percepcoes}\n'
+        string += f'Ouro: {self.ouro}\n'
+        string += f'Game Over: {self.game_over}\n'
+        string += f'Casa: {self.casa}\n'
+        string += f'Capturado: {self.capturado}\n'
+        string += f'Caiu: {self.caiu}\n'
+        string += f'Passos: {self.passos}\n'
+        string += f'Matou? {self.matou}\n'
+        string += f'Tiros: {self.tiros}\n'
+        return string
+        
