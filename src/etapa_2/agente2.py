00f14b1af83a5a60e4f92254fbfb9829e74e8ed4
Antonio Carlos <jhunny.tec@gmail.com>
2023-05-20 00:12:17 -0300

    alterações no agente 2

--- src/etapa_2/agente2.py
+++ src/etapa_2/agente2.py
@@ -1,7 +1,7 @@
 from mapa import Mapa
 import random
 
-class Agente(): 
+class Agente():
     def __init__(self, mapa:Mapa):
         self.x = 0
         self.y = 0
@@ -9,6 +9,7 @@ class Agente():
         self.mapa.posicao_agente(self.x, self.y)
         self.percepcoes, self.caminhos, self.casa = mapa.info(self.x, self.y)
         self.flecha = 1
+        self.medo = 7        # de 1 a 10
         
         self.game_over = False
         self.ouro = 0
@@ -17,7 +18,7 @@ class Agente():
         self.passos = 0
         self.matou = False
         self.tiros = 0
-        
+        self.memoria = []
 
     def acao(self):
         percepcoes = self.percepcoes
@@ -43,10 +44,28 @@ class Agente():
 
     def caminha(self):
         proximos = self.caminhos
-        proximo = random.randint(0, len(proximos) - 1 )
-        proxima_casa = proximos[proximo]
 
+        conhecidos = []
+        for casa in proximos:
+            if casa in proximos:
+                conhecidos.append(casa)
+
+        # print('conhecidos: ', conhecidos)
+        coragem = random.randint(1, 10)
+
+        if coragem >= self.medo or conhecidos == []:
+            proximo = random.randint(0, len(proximos) - 1 )
+            proxima_casa = proximos[proximo]
+        
+        else:
+            proximo = random.randint(0, len(conhecidos) - 1 )
+            proxima_casa = conhecidos[proximo]
+
+        posicao = [self.x, self.y]
+        self.memoria.append(posicao)
         self.mapa.esvazia(self.x, self.y)
+
+
 
         self.x = proxima_casa[0]
         self.y = proxima_casa[1]
@@ -89,5 +108,9 @@ class Agente():
         string += f'Passos: {self.passos}\n'
         string += f'Matou? {self.matou}\n'
         string += f'Tiros: {self.tiros}\n'
+        
+        if self.memoria != []:
+            string += f'Memória: {self.memoria}\n'
+        
         return string
         
